4fe6de4676dbb5e189623bda8884194ad346fe99
Leonardo Gabriel <leomour@outlook.com>
2024-08-26 20:14:36 -0300

    segundo commit

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '8119',
+  database: 'postgres',
+  define: {
+    timestamps: true,
+    underscored: true
+  }
+}
